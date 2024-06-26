251b897cb4fd69e0f381f422706c1768260057eb
Matheus Brizolla <matheusfnx@hotmail.com>
2024-06-26 22:25:39 +0100

    feat: new features

---
+++ {{cookiecutter.__folder_name}}/src/streamlit_app/pages/data_analysis.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+from utils.data_loader import load_data
+from utils.data_processor import process_data
+
+
+def app():
+    st.title("Data Analysis")
+    data = load_data()
+    processed_data = process_data(data)
+    st.dataframe(processed_data)
---
+++ {{cookiecutter.__folder_name}}/src/streamlit_app/pages/home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+def app():
+    st.title("Welcome to Our Data Analysis App")
+    st.write(
+        "This app demonstrates various data analysis and visualization techniques."
+    )
+    st.write("Use the sidebar to navigate between different pages.")
