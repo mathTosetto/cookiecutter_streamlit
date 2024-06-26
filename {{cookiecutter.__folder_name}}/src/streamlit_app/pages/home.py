e420c8e5dc2946dce5eee466a10376133f97be65
Matheus Brizolla <matheusfnx@hotmail.com>
2024-06-26 23:40:24 +0100

    refactor: remove old files

--- {{cookiecutter.__folder_name}}/src/streamlit_app/pages/home.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-
-def app():
-    st.title("Welcome to Our Data Analysis App")
-    st.write(
-        "This app demonstrates various data analysis and visualization techniques."
-    )
-    st.write("Use the sidebar to navigate between different pages.")
--- {{cookiecutter.__folder_name}}/src/streamlit_app/utils/data_processor.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-def process_data(df):
-    # This is a simple example.
-    df["feature3"] = df["feature1"] * df["feature2"]
-    return df
