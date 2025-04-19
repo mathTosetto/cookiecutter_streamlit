89b67e07a396adf2dc05af2a240cec0654d824d1
Matheus Brizolla <22848645+mathTosetto@users.noreply.github.com>
2025-04-19 19:36:02 +0100

    refactor: init the logger setup

--- {{ cookiecutter.project_slug }}/app.py
+++ {{ cookiecutter.project_slug }}/app.py
@@ -1,10 +1,16 @@
 import streamlit as st
+
+from src.utils.my_logger import LoggerSetup
 from src.streamlit_app.pages import home, data_analysis, visualization
+
 
 PAGES = {"Home": home, "Data Analysis": data_analysis, "Visualization": visualization}
 
 
 def main():
+    # Initialize the logger setup
+    LoggerSetup()
+
     st.sidebar.title("Navigation")
     selection = st.sidebar.radio("Go to", list(PAGES.keys()))
     page = PAGES[selection]
