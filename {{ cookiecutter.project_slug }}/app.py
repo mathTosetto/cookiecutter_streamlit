c82b2f63dca48c4146513d451504914ee999a9f8
Matheus Brizolla <22848645+mathTosetto@users.noreply.github.com>
2025-04-19 19:41:41 +0100

    refactor: init the logger and var envs

--- {{ cookiecutter.project_slug }}/app.py
+++ {{ cookiecutter.project_slug }}/app.py
@@ -1,16 +1,24 @@
+import logging
+
 import streamlit as st
+
+from dotenv import load_dotenv
 
 from src.utils.my_logger import LoggerSetup
 from src.streamlit_app.pages import home, data_analysis, visualization
 
+# Initialize logger setup
+LoggerSetup()
+
+# Load environment variables
+load_dotenv()
+
+LOGGER: logging.Logger = logging.getLogger("main")
 
 PAGES = {"Home": home, "Data Analysis": data_analysis, "Visualization": visualization}
 
 
 def main():
-    # Initialize the logger setup
-    LoggerSetup()
-
     st.sidebar.title("Navigation")
     selection = st.sidebar.radio("Go to", list(PAGES.keys()))
     page = PAGES[selection]
